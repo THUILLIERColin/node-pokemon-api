db2335c02baab12558e2fdc6395cc1b55e7aaa3c
THUILLIERColin <colin.thuillier@gmail.com>
2024-08-08 16:58:27 +0200

    feat: enhance app with new middleware and routes
    - Install and configure morgan, serve-favicon, and body-parser
    - Update app.js to include middleware for logging, serving favicon, and parsing JSON
    - Add new routes for creating, updating, and deleting pokemons
    - Modify existing routes to use helper functions for consistent responses
    - Update package.json and package-lock.json to include new dependencies

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 let pokemons = require('./mock-pokemon');
-const { success } = require('./helper');
+const { success, getUniqueID } = require('./helper');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+
+app
+ .use(favicon(__dirname + '/favicon.ico'))
+ .use(morgan('dev'))
+ .use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello Express !'));
 
@@ -17,5 +25,32 @@ app.get('/api/pokemons/:id', (req, res) => {
 app.get('/api/pokemons', (req, res) => {
     res.json(success('La liste des pokémons à bien été récupéré', pokemons));
 })
+
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueID(pokemons);
+    const pokemonCreated = { ...req.body, ...{ id, created: new Date() } };
+    pokemons.push(pokemonCreated);
+    res.json(success(`Le pokémon ${pokemonCreated.name} a bien été créé`, pokemonCreated));
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let pokemonToUpdate = pokemons.find(pokemon => pokemon.id === id);
+    const pokemonUpdated = { id: id, ...req.body, created: pokemonToUpdate.created };
+    pokemons = pokemons.map(pokemon => {
+     return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+     
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+   });
+
+   app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
+  });
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
--- helper.js
+++ helper.js
@@ -1,6 +1,13 @@
+const pokemons = require("./mock-pokemon");
+
 exports.success = (message, data) => {
   return {
     message,
     data,
   };
-};
+}
+
+exports.getUniqueID = (pokemons) => {
+    const pokemonIds = pokemons.map(pokemon => pokemon.id);
+    return pokemonIds.length > 0 ? Math.max(...pokemonIds) + 1 : 1;
+}
