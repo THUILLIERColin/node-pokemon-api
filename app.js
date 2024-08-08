e528e4225438ef67a434546323796bc74ad2ec05
THUILLIERColin <colin.thuillier@gmail.com>
2024-08-08 15:21:40 +0200

    feat: add helper.js and update app.js with new helper function
    - Create helper.js with a success function for consistent response formatting
    - Update app.js to use the success function for API responses
    - Modify routes to return JSON responses with messages and data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let pokemons = require('./mock-pokemon');
+const { success } = require('./helper');
 
 const app = express();
 const port = 3000;
@@ -9,11 +10,12 @@ app.get('/', (req, res) => res.send('Hello Express !'));
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find(pokemon => pokemon.id === id);
-    res.send(`Voici le pokémon demandé : ${pokemon.name}`);
+    const message = 'Un pokémon a été trouvé';
+    res.json(success(message, pokemon));
 })
 
 app.get('/api/pokemons', (req, res) => {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex`);
+    res.json(success('La liste des pokémons à bien été récupéré', pokemons));
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
