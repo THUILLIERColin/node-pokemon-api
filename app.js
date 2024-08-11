cd84adcbe21a7694ad1468bc642e52bdc701eb87
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-08-11 17:52:41 +0200

    feat: update route handling and error management across all routes
    - Refactor and improve all route handlers in app.js and respective route files
    - Enhance error handling and response messages in createPokemon.js, deletePokemon.js, findAllPokemons.js, findPokemonByPk.js, and updatePokemon.js
    - Ensure consistent use of Sequelize for database operations across all routes
    - Update app.js to properly integrate the refactored routes

--- app.js
+++ app.js
@@ -14,10 +14,18 @@ app
 
 sequelize.initDb()
 
+// Endpoint
 require('./src/routes/findAllPokemons')(app);
 require('./src/routes/findPokemonByPk')(app);
 require('./src/routes/createPokemon')(app);
 require('./src/routes/updatePokemon')(app);
 require('./src/routes/deletePokemon')(app);
+
+// Handling 404 errors
+
+app.use((req, res, next) => {
+    const message = `Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.`;
+    res.status(404).json({ message });
+});
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
